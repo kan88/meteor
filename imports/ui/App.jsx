509ff76dbe175e1eaee4b3feb9abb649c82c3a47
Евгений <msk9199625687@yandex.ru>
2023-11-25 18:31:06 +0300

    feat: mongo connect

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,18 +1,37 @@
 import React from "react";
 import { useTracker } from "meteor/react-meteor-data";
-import { TasksCollection } from "/imports/api/TasksCollection";
 import { Task } from "./Task";
+import { TasksCollection } from "/imports/api/TasksCollection";
+import { TaskForm } from "./TaskForm";
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() =>
+    TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+  );
 
+  const toggleChecked = ({ _id, isChecked }) => {
+    TasksCollection.update(_id, {
+      $set: {
+        isChecked: !isChecked,
+      },
+    });
+  };
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
 
+      <TaskForm />
+
       <ul>
         {tasks.map((task) => (
-          <Task key={task._id} task={task} />
+          <Task
+            key={task._id}
+            task={task}
+            onCheckboxClick={toggleChecked}
+            onDeleteClick={deleteTask}
+          />
         ))}
       </ul>
     </div>
